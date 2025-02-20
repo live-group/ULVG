2a8b4676ada9dd07d764b4919ecf02850d942192
live-group <132337501+live-group@users.noreply.github.com>
2025-02-20 22:27:44 +0800

    Initial commit

--- train.sh
+++ train.sh
@@ -1,2 +1,3 @@
 CUDA_VISIBLE_DEVICES=0 python train_fasterrcnn_clip.py --config-file configs/diverse_weather_fasterrcnn_clip.yaml \
-OUTPUT_DIR "all_outs/train/log" SOLVER.MAX_ITER 240000 SOLVER.STEPS [160000,] DATASETS.TEST "('daytime_foggy_train','dusk_rainy_train',)"
+OUTPUT_DIR "all_outs/train/log" SOLVER.MAX_ITER 240000 SOLVER.STEPS [160000,] \
+DATASETS.TEST "('daytime_foggy_train','night_sunny_train','night_rainy_train','dusk_rainy_train',)"
